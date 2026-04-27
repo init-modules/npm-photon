d484be7abb3b3b78eed39b43d691303d4ffea41a
Vlad <vladislavkadatskikh6@gmail.com>
2026-04-28 01:32:58 +0500

    chore: tighten .gitignore and untrack build artifacts

--- dist/link-url-XwgBLvA0.d.ts
+++
@@ -1,6 +0,0 @@
-declare const decodePhotonHtmlEntities: (value: string) => string;
-declare const normalizePhotonUrlForProtocolCheck: (value: string) => string;
-declare const sanitizePhotonLinkHref: (href: unknown, fallback?: string) => string;
-declare const getPhotonAnchorRel: (target: unknown, rel: unknown) => string | undefined;
-
-export { decodePhotonHtmlEntities as d, getPhotonAnchorRel as g, normalizePhotonUrlForProtocolCheck as n, sanitizePhotonLinkHref as s };
--- dist/surface-layout-Dv4KKqe3.d.ts
+++
@@ -1,6 +0,0 @@
-import { CSSProperties } from 'react';
-import { al as PhotonSurfaceMode } from './types-C1q0pf4n.js';
-
-declare const getPhotonSurfaceModeStyle: (mode: PhotonSurfaceMode) => CSSProperties | undefined;
-
-export { getPhotonSurfaceModeStyle as g };
